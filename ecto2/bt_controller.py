853cae65ec9cd7c291798e1b1d5acaac78b18816
shayne <shaynexwang@gmail.com>
2020-02-29 19:09:42 -0800

    fixed controller mappingwq

--- ecto2/bt_controller.py
+++ ecto2/bt_controller.py
@@ -13,28 +13,34 @@ class BTController(Node):
             automatically_declare_parameters_from_overrides=True,
     )
     self.joy_sub = self.create_subscription(Joy, "joy", self.joyCallback, 1)
+
     self.steering_pub = self.create_publisher(Float64, 'ecto2/steering', 1) 
     self.throttle_pub = self.create_publisher(Float64, 'ecto2/throttle', 1) 
-    self.breaking_pub = self.create_publisher(Float64, 'ecto2/breaking', 1) 
+    self.breaking_pub = self.create_publisher(Int32, 'ecto2/breaking', 1) 
 
   def joyCallback(self, joy_msg):
     msg = Float64()
     button_msg = Int32()
 
     steering_cmd = joy_msg.axes[self.get_parameter("LEFT_JOY_LR").value]
-    msg.data = steering_cmd
-    self.get_logger().info('Publishing: "%s"' % msg.data)
+    msg.data = 100*(steering_cmd)
+    #self.get_logger().info('Publishing: "%s"' % msg.data)
     self.steering_pub.publish(msg)
 
-    throttle_cmd = joy_msg.axes[self.get_parameter("LEFT_JOY_UD").value]
-    msg.data = throttle_cmd
-    self.get_logger().info('Publishing: "%s"' % msg.data)
+   #  throttle_cmd = joy_msg.axes[self.get_parameter("LEFT_JOY_UD").value]
+   #  msg.data = throttle_cmd
+   #  #self.get_logger().info('Publishing: "%s"' % msg.data)
+   #  self.throttle_pub.publish(msg)
+
+    lt_val = joy_msg.axes[self.get_parameter("LT").value]
+    rt_val = joy_msg.axes[self.get_parameter("RT").value]
+    msg.data = 100*(lt_val - rt_val)
     self.throttle_pub.publish(msg)
 
     breaking_cmd= joy_msg.buttons[self.get_parameter("B").value]
     button_msg.data = breaking_cmd
-    self.get_logger().info('Publishing: "%s"' % msg.data)
-    self.breaking_pub.publish(msg)
+    #self.get_logger().info('Publishing: "%s"' % msg.data)
+    self.breaking_pub.publish(button_msg)
 
 def main(args=None):
   rclpy.init(args=args)
